d2be668c047f8cf1fbfbf7dadc5d6012db3e2031
Nicowkra <nicow642@gmail.com>
2026-04-21 14:46:19 -0300

    Tarea 2

--- njjn.py
+++ njjn.py
@@ -1,33 +1,17 @@
-import random
-from collections import Counter
-def generador(nombre:list, numeros:list):
-    #CONSTRUYO EL DICT
-    dic = {}
-    for i in range(len(nombre)):
-        dic[nombre[i]] = numeros.copy()
-    for i in range(len(numeros)):
-        dic[numeros[i]] = nombre.copy()
-    
-    for x in range(len(nombre)):
-        permitidos = []
-        for j in range(len(nombre)):
-            if (x == len(nombre)-1 and (nombre[j] != nombre[x])):
-                permitidos.append(nombre[j])
-                
+def primos2_modificado(limite):
+    lista = []
+    n = 2
+    while (n < limite):
+        m = int((n)**0.5)
+        while (m > 0):
+            if (m == 1): 
+                lista.append(n)
+                m = 0
+                n += 1
+            elif ((n % m) == 0):
+                n += 1
+                m = 0
             else:
-                if (nombre[j] != nombre[x+1]) and (nombre[j] != nombre[x]):
-                    permitidos.append(nombre[j])
-        dic[nombre[x]] += permitidos
-    print(dic)
-    for y in range(len(numeros)):
-        permitidos = []
-        for j in range(len(numeros)):
-            if numeros[j] != numeros[y+1]:
-                permitidos.append(numeros[j])
-        dic[numeros[x]] += permitidos
-
-generador(["h","o","l"],[1,4,6,4,1,3])
-
-
-    
-    
+                m -= 1
+    return lista
+print(primos2_modificado(1000))
